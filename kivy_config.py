6a979c9ebef16ab45c49f08e283062860d3a1643
xneve <michalsnieg20@gmail.com>
2023-12-15 00:45:44 +0100

    start

---
+++ kivy_config.py
@@ -0,0 +1,10 @@
+from kivy.config import Config
+
+if __name__ != '__main__':
+    Config.set('kivy', 'window_icon', 'img/icon/icon.png')
+
+    Config.set('graphics', 'resizable', '0')
+    # Config.set('graphics', 'width', '1080')
+    # Config.set('graphics', 'height', '2340')
+    Config.set('graphics', 'width', '540')
+    Config.set('graphics', 'height', '1140')
---
+++ main.py
@@ -0,0 +1,117 @@
+import kivy
+import kivy_config
+
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class HomeWindow(Screen):
+    def __init__(self, **kwargs):
+        super(HomeWindow, self).__init__(**kwargs)
+        # self.layout.add_widget(NavBar())
+
+class ProfileWindow(Screen):
+    def __init__(self, **kwargs):
+        super(ProfileWindow, self).__init__(**kwargs)
+
+class TrainWindow(Screen):
+    def __init__(self, **kwargs):
+        super(TrainWindow, self).__init__(**kwargs)
+
+class NutritionWindow(Screen):
+    def __init__(self, **kwargs):
+        super(NutritionWindow, self).__init__(**kwargs)
+
+class StatsWindow(Screen):
+    def __init__(self, **kwargs):
+        super(StatsWindow, self).__init__(**kwargs)
+
+class NavBar(Widget):
+    def __init__(self, manager, current, **kwargs):
+        super(NavBar, self).__init__(**kwargs)
+
+        home_image = './img/house-gray.png'
+        train_image = './img/barbell-gray.png'
+        diet_image = './img/carrot-gray.png'
+        stats_image = './img/chart-bar-gray.png'
+
+        match current.name:
+            case 'home':
+                home_image = './img/house-white.png'
+            case 'train':
+                train_image = './img/barbell-white.png'
+            case 'diet':
+                diet_image = './img/carrot-white.png'
+            case 'stats':
+                stats_image = './img/chart-bar-white.png'
+            case _:
+                pass
+
+        home_button = NavButton('home', home_image)
+        train_button = NavButton('train', train_image)
+        diet_button = NavButton('diet', diet_image)
+        stats_button = NavButton('stats', stats_image)
+
+        self.buttons = [home_button, train_button, diet_button, stats_button]
+        for button in self.buttons:
+            self.layout.add_widget(button)
+
+class NavButton(Widget):
+    def __init__(self, window, img, **kwargs):
+        super(NavButton, self).__init__(**kwargs)
+        self.window = window
+        self.button.background_normal = img
+        self.button.background_down = img
+
+class Separator(Widget):
+    pass
+
+class TopBar(Widget):
+    pass
+
+class NotificationBell(Widget):
+    pass
+
+class ProfileIcon(Widget):
+    pass
+
+class Content(Widget):
+    pass
+
+class WindowManager(ScreenManager):
+    def __init__(self, **kwargs):
+        super(WindowManager, self).__init__(**kwargs)
+
+class MainApp(App):
+    def __init__(self, manager, **kwargs):
+        super(MainApp, self).__init__(**kwargs)
+        self.manager = manager
+
+    def build(self):
+        self.title = 'MMS'
+        return self.manager
+
+if __name__ == '__main__':
+    kv = Builder.load_file('structure.kv')
+    manager = WindowManager()
+
+    home_window = HomeWindow(name='home')
+    train_window = TrainWindow(name='train')
+    diet_window = NutritionWindow(name='diet')
+    stats_window = StatsWindow(name='stats')
+
+    main_screens = [home_window, train_window, diet_window, stats_window]
+    for screen in main_screens:
+        screen.add_widget(TopBar())
+        screen.add_widget(Content())
+        screen.add_widget(NavBar(manager=manager, current=screen))
+        manager.add_widget(screen)
+
+    manager.current = 'home'
+
+    profile_window = ProfileWindow(name='profile')
+    manager.add_widget(profile_window)
+
+    MainApp(manager).run()
