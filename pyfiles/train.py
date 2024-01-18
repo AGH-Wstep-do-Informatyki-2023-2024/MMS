f20cb61a6c98c74c7cb168cfe6317755264b5275
xneve <michalsnieg20@gmail.com>
2024-01-19 00:59:54 +0100

    jest pare bugow ale to sie ogarnie

--- pyfiles/train.py
+++ pyfiles/train.py
@@ -16,6 +16,12 @@ IMG_PATH = './img/'
 nt_first = None
 nt_second = None
 
+ne_window = None
+nt_window = None
+
+configuration = {}
+exercises = []
+
 class TrainLabel(Widget):
     def __init__(self, id, **kwargs):
         super(TrainLabel, self).__init__(**kwargs)
@@ -44,12 +50,150 @@ class BackButton(Button):
 
         if self.previous == 'train':
             global nt_first
+            global nt_second
             nt_first.reset_first()
+            nt_second.reset_second()
+            
+            global configuration
+            configuration = {}
 
+            global exercises
+            exercises = []
+
+            # print(configuration)
+
+            # print(exercises)
+
+        if self.previous == 'train_new_training_second':
+            global ne_window
+            global nt_window
+
+            ne_window.reset()
+            nt_window.reset()
+            
+
+class NewExerciseLabel(BoxLayout):
+    def __init__(self, **kwargs):
+        super(NewExerciseLabel, self).__init__(**kwargs)
+
+class NewExerciseMenu(BoxLayout):
+    def __init__(self, **kwargs):
+        super(NewExerciseMenu, self).__init__(**kwargs)
+    
+class AddExercise(Button):
+    def __init__(self, **kwargs):
+        super(AddExercise, self).__init__(**kwargs)
+
+    def add_exercise(self):
+        global manager
+        manager.current = 'new_exercise'
+
+    def add_timer(self):
+        global manager
+        manager.current = 'new_timer'
+
+class NewTimerWindow(Screen):
+    def __init__(self, **kwargs):
+        super(NewTimerWindow, self).__init__(**kwargs)
+        self.reset()
+
+    def reset(self):
+        self.content = NewTimerContent()
+        self.add_widget(self.content)
+
+class NewTimerContent(GridLayout):
+    def __init__(self, **kwargs):
+        super(NewTimerContent, self).__init__(**kwargs)
+
+class NewExerciseWindow(Screen):
+    def __init__(self, **kwargs):
+        super(NewExerciseWindow, self).__init__(**kwargs)
+        self.content = None
+        self.reset()
+
+    def reset(self):
+        if self.content:
+            self.content.clear_widgets()
+        self.content = NewExerciseContent()
+        self.add_widget(self.content)
+
+
+class NewExerciseContent(GridLayout):
+    def __init__(self, **kwargs):
+        super(NewExerciseContent, self).__init__(**kwargs)
+        self.tbb.layout.add_widget(BackButton('train_new_training_second'))
+
+    def add_exercise(self):
+        if self.ex_name.text.strip() == '':
+            return
+        
+        global exercises
+        new_ex = {
+            'name': self.ex_name.text.strip(),
+            'description': self.ex_description.text.strip()
+            }
+        exercises.append(new_ex)
+        
+        global manager
+        manager.current = 'train_new_training_second'
+
+        global nt_second
+        nt_second.content.reset()
+
+        self.ex_name.text = ''
+        self.ex_description.text = ''
+
+        # print(exercises)
+        
 
 class NewTrainingSecondContent(GridLayout):
     def __init__(self, **kwargs):
         super(NewTrainingSecondContent, self).__init__(**kwargs)
+
+        global manager
+        if manager.has_screen('new_exercise'):
+            manager.remove_widget(manager.get_screen('new_exercise'))
+
+        if manager.has_screen('new_timer'):
+            manager.remove_widget(manager.get_screen('new_timer'))
+
+        global ne_window
+        global nt_window
+            
+        ne_window = NewExerciseWindow(name='new_exercise')
+        nt_window = NewTimerWindow(name='new_timer')
+        manager.add_widget(ne_window)
+        manager.add_widget(nt_window)
+
+        self.tbb.layout.add_widget(BackButton('train_new_training_first'))
+
+        self.reset()
+
+    def add_object(self, object):
+        self.content_layout.add_widget(object)
+
+        self.num_of_elements += 1
+        self.content_layout.height += object.height
+
+    def reset(self):
+        self.num_of_elements = 0
+        self.content_layout.height = self.content_layout.minimum_height
+        self.content_layout.clear_widgets()
+
+        global exercises
+
+        print(exercises)
+        global manager
+        print(manager.screens)
+        for ex in exercises:
+            new_ex = NewExerciseLabel()
+            new_ex.img.source = IMG_PATH + 'barbell-white.png'
+            new_ex.label.text = ex['name']
+            self.add_object(new_ex)
+        
+        self.add_object(NewExerciseMenu())
+
+        self.add_object(Label(text=''))
 
 
 class NewTrainingFirstContent(GridLayout):
@@ -59,7 +203,28 @@ class NewTrainingFirstContent(GridLayout):
         self.tbb.layout.add_widget(BackButton('train'))
         self.change_icon(None)
 
+    def go_next(self, next):
+        if self.name.text.strip() == '':
+            return
+        
+        if not self.current_icon:
+            return 
+
+        global manager
+        manager.current = next
+
+        global configuration
+        configuration['name'] = self.name.text.strip()
+        configuration['icon'] = self.current_icon
+        configuration['description'] = self.description.text.strip()
+
+        global nt_second 
+        nt_second.content.name_label.text = self.name.text.strip()
+
+        # print(configuration)
+
     def change_icon(self, icon):
+        self.current_icon = icon
         self.icons.clear_widgets()
 
         barbell = IMG_PATH + 'barbell-gray.png'
@@ -116,15 +281,26 @@ class NewTrainingFirst(Screen):
         self.content = NewTrainingFirstContent()
         self.add_widget(self.content)
 
+        # global nt_first
+        # global manager
+        # manager.remove_widget(nt_first)
+
+
 
 class NewTrainingSecond(Screen):
     def __init__(self, **kwargs):
         super(NewTrainingSecond, self).__init__(**kwargs)
+        self.reset_second()
 
-    def reset(self):
-        self.clear_widgets()
-        self.add_widget(TopBackButton('train_new_training_first'))
-        print('reset 2')
+    def reset_second(self):
+        self.content = NewTrainingSecondContent()
+        self.add_widget(self.content)
+
+        self.content.reset()
+
+        # global nt_second
+        # global manager
+        # manager.remove_widget(nt_second)
 
 class DarkTextInput(TextInput):
     def __init__(self, **kwargs):
